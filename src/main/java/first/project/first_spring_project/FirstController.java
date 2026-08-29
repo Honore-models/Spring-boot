59226562b1f970dbe6ad653c54abf8c9095c1d8d
honore <honoreniyogushimwa63@gmail.com>
2026-08-29 18:26:54 +0200

    initial commit

---
+++ src/main/java/first/project/first_spring_project/FirstController.java
@@ -0,0 +1,14 @@
+package first.project.first_spring_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "index.html";
+    }
+
+}
