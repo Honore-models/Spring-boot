0a6204bfde9bf70185b8a95cd1f2b34c0ccb594f
honore <honoreniyogushimwa63@gmail.com>
2026-09-01 12:41:31 +0200

    refactor

--- src/main/java/first/project/first_spring_project/FirstSpringProjectApplication.java
+++ src/main/java/first/project/first_spring_project/FirstSpringProjectApplication.java
@@ -2,11 +2,9 @@ package first.project.first_spring_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-import first.project.first_spring_project.learningClasses.FirstService;
-import first.project.first_spring_project.learningClasses.MyFirstClass;
-
+// import org.springframework.core.env.Environment;
+// import first.project.first_spring_project.learningClasses.FirstService;
+// import first.project.first_spring_project.learningClasses.MyFirstClass;
 // import first.project.first_spring_project.Services.OrderService;
 // import first.project.first_spring_project.Services.PaypalService;
 // import first.project.first_spring_project.Services.StripePamentService;
@@ -15,24 +13,21 @@ import first.project.first_spring_project.learningClasses.MyFirstClass;
 public class FirstSpringProjectApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(FirstSpringProjectApplication.class, args);
 
-		MyFirstClass myFirstClass = ctx.getBean("firstBean", MyFirstClass.class);
-		System.out.println(myFirstClass.sayHero());
+		SpringApplication.run(FirstSpringProjectApplication.class, args);
 
-		FirstService firstService = ctx.getBean(FirstService.class);
-		firstService.sayHero();
-
-		Environment env = ctx.getBean(Environment.class);
-		System.out.println(env.getProperty("os.name"));
-		System.out.println(env.getProperty("my.custom.property"));
-
-		System.out.println(firstService.getjavaVersion());
-		System.out.println(firstService.getOsName());
-
+		// var ctx = SpringApplication.run(FirstSpringProjectApplication.class, args);
+		// MyFirstClass myFirstClass = ctx.getBean("firstBean", MyFirstClass.class);
+		// System.out.println(myFirstClass.sayHero());
+		// FirstService firstService = ctx.getBean(FirstService.class);
+		// firstService.sayHero();
+		// Environment env = ctx.getBean(Environment.class);
+		// System.out.println(env.getProperty("os.name"));
+		// System.out.println(env.getProperty("my.custom.property"));
+		// System.out.println(firstService.getjavaVersion());
+		// System.out.println(firstService.getOsName());
 		// OrderService orderService = new OrderService(new StripePamentService());
 		// orderService.placeOrder();
-
 		// OrderService orderService2 = new OrderService(new PaypalService());
 		// orderService2.placeOrder();
 	}
