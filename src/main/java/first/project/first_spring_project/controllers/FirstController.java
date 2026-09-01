bb953196247fb4be5590ae99dc17856ae69ed324
honore <honoreniyogushimwa63@gmail.com>
2026-09-01 19:50:18 +0200

    refactor

--- src/main/java/first/project/first_spring_project/controllers/FirstController.java
+++ src/main/java/first/project/first_spring_project/controllers/FirstController.java
@@ -2,8 +2,10 @@ package first.project.first_spring_project.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,18 @@ public class FirstController {
     @PostMapping("/post-order")
     public String postOrder(@RequestBody Order order) {
         return "Posted successfully and the order is: " + order;
+    }
+
+    @GetMapping("/product/{productName}")
+    public String pathVar(@PathVariable("productName") String name) {
+        return "Your product name is: " + name;
+    }
+
+    @GetMapping("/hello")
+    public String requestParams(
+            @RequestParam("name") String name,
+            @RequestParam("age") String age) {
+        return "Hello " + name + " you are " + age + " years old";
     }
 
 }
