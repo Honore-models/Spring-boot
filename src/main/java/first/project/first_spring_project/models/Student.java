423d6b2a1d37dd3d09c1f01cd1c96b413059fc84
honore <honoreniyogushimwa63@gmail.com>
2026-09-02 10:29:07 +0200

    feat: added first JPA and hibernate

---
+++ src/main/java/first/project/first_spring_project/models/Student.java
@@ -0,0 +1,98 @@
+package first.project.first_spring_project.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Students")
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name = "fname")
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int age;
+    private String address;
+    private String city;
+
+    public Student() {
+    } // For hibernate
+
+    public Student(String firstName, String lastName, String email, String password, int age, String address, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.address = address;
+        this.city = city;
+    } // For the developer
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
